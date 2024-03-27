994eca4e2cbf8b325f9540766686b0f60bd681d8
Howl Kimura <howlkimuradev@gmail.com>
2024-03-27 15:27:14 +0300

    Create a game_details page view with a get router for it

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -5,7 +5,7 @@ const asyncHandler = require("express-async-handler");
 const { body, validationResult } = require("express-validator");
 
 // GET all games on /games
-exports.games =  asyncHandler(async (req, res, next) => {
+exports.games_list_get =  asyncHandler(async (req, res, next) => {
   const games = await Game.find({}, "title studio img_path")
   .sort({title: 1})
   .populate("studio")
@@ -15,5 +15,29 @@ exports.games =  asyncHandler(async (req, res, next) => {
   res.render("games", {
     title: "Games Collection",
     games: games
+  })
+})
+
+// GET game details on /games/id
+exports.game_details_get =  asyncHandler(async (req, res, next) => {
+  const game = await Game.findById(req.params.id)
+  .populate('category')
+  .populate('studio')
+  .exec()
+  .catch(() => {
+    const err = new Error("Game not found");
+    err.status = 404;
+    return next(err);
+  });
+ 
+  if (game === null) {
+    // No results.
+    const err = new Error("Game not found");
+    err.status = 404;
+    return next(err);
+  }
+
+  res.render("game_details", {
+    game
   })
 })
--- routes/games.js
+++ routes/games.js
@@ -4,6 +4,9 @@ var router = express.Router();
 const game_controller = require('../controllers/gameController');
 
 /* GET games listing. */
-router.get('/', game_controller.games );
+router.get('/', game_controller.games_list_get );
+
+/* GET game details. */
+router.get('/:id', game_controller.game_details_get );
 
 module.exports = router;
