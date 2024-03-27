1ee490b6d7bf021db681060be9811f62683d33a7
Howl Kimura <howlkimuradev@gmail.com>
2024-03-27 10:27:24 +0300

    Fix bug in poplulate script and populate the db with 2 games

--- populate.js
+++ populate.js
@@ -18,7 +18,7 @@ const studios = [];
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 
-const mongoDB = userArgs[0];
+const mongoDB = "mongodb+srv://heresywolves:mACSNDL7e3BRZGTO@cluster0.avuxlqr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 main().catch((err) => console.log(err));
 
@@ -27,8 +27,8 @@ async function main() {
   await mongoose.connect(mongoDB);
   console.log("Debug: Should be connected?");
   await createCategories();
-  await createGames();
   await createStudios();
+  await createGames();
   console.log("Debug: Closing mongoose");
   mongoose.connection.close();
 }
@@ -67,22 +67,8 @@ async function createCategories() {
   await Promise.all([
     categoryCreate(0, "Third-person shooter"),
     categoryCreate(1, "Mecha"),
-  ]);
-}
-
-async function createGames() {
-  console.log("Adding games");
-  await Promise.all([
-    gameCreate(0, 
-      "Armored Core VI: Fires of Rubicon",
-      studios[0], 
-      new Date('August 25, 2023'), 
-      "The augmented mercenary C4-621 is smuggled onto Rubicon with the assistance of their boss, Handler Walter. Upon landing on Rubicon, C4-621 steals the callsign of a deceased mercenary, becoming "Raven". With the new identity, Raven inserts themselves into the war raging on Rubicon's surface between the megacorporations Balam Industries and Arquebus Corp, the anti-corporation Rubicon Liberation Front (RLF), and the Planetary Closure Administration (PCA), who are all racing to find or protect the Coral. During numerous battles, Raven ends up befriending V.IV Rusty, an elite AC pilot working for Arquebus's Vespers unit; "Cinder" Carla, leader of RaD, a black market arms dealing company; and G1 Michigan, the leader of Balam's Redgun unit who treats Raven as an honorary member. They occasionally provide assistance directly or through comms. In addition, Raven encounters G5 Iguazu, a Redgun AC pilot who envies Raven's exploits and develops a one-sided rivalry with them; and V.II Snail, the leader of Arquebus's Vespers who looks down on Raven due to his superiority complex. Later, Raven assaults the PCA facility and is caught in a Coral surge, resulting in them making contact with an entity named Ayre who can speak directly to their mind.",
-      [categories[0], categories[1]],
-      50,
-      59.99,
-      0
-    ),
+    categoryCreate(2, "Survival"),
+    categoryCreate(3, "Horror"),
   ]);
 }
 
@@ -93,5 +79,36 @@ async function createStudios() {
       "FromSoftware",
       "FromSoftware, Inc. is a Japanese video game development and publishing company. It was founded by Naotoshi Zin in Tokyo on November 1, 1986. Initially a developer of business software, the company released their first video game, King's Field, for the PlayStation in 1994."
     ),
+    studioCreate(1,
+      "Zeekerss",
+      "Zeekerss isn’t a ‘studio’ but the solo developer behind the indie horror game Lethal Company and at the time of its release in October 2023, Zeekerss was 21 years old. This news can be surprising because the gameplay, mechanics, and features of the game do not look like it’s made by someone alone, especially since the updates are pretty regular and well done. The title is still in early access at the time of writing this article but already has 250K+ “Overwhelmingly Positive” reviews which is a tough feat, even for established devs and AAA publishers.", 
+    )
   ]);
 }
+
+async function createGames() {
+  console.log("Adding games");
+  await Promise.all([
+    gameCreate(0, 
+      "Armored Core VI: Fires of Rubicon",
+      studios[0], 
+      new Date('August 25, 2023'), 
+      "The augmented mercenary C4-621 is smuggled onto Rubicon with the assistance of their boss, Handler Walter. Upon landing on Rubicon, C4-621 steals the callsign of a deceased mercenary, becoming \"Raven\". With the new identity, Raven inserts themselves into the war raging on Rubicon's surface between the megacorporations Balam Industries and Arquebus Corp, the anti-corporation Rubicon Liberation Front (RLF), and the Planetary Closure Administration (PCA), who are all racing to find or protect the Coral. During numerous battles, Raven ends up befriending V.IV Rusty, an elite AC pilot working for Arquebus's Vespers unit; \"Cinder\" Carla, leader of RaD, a black market arms dealing company; and G1 Michigan, the leader of Balam's Redgun unit who treats Raven as an honorary member. They occasionally provide assistance directly or through comms. In addition, Raven encounters G5 Iguazu, a Redgun AC pilot who envies Raven's exploits and develops a one-sided rivalry with them; and V.II Snail, the leader of Arquebus's Vespers who looks down on Raven due to his superiority complex. Later, Raven assaults the PCA facility and is caught in a Coral surge, resulting in them making contact with an entity named Ayre who can speak directly to their mind.",
+      [categories[0], categories[1]],
+      50,
+      59.99,
+      0
+    ),
+    gameCreate(1,
+      "Lethal Company",
+      studios[1],
+      new Date('October 23, 2023'),
+      "In Lethal Company, players obtain and sell scrap from abandoned, industrialized exomoons while avoiding traps, environmental hazards, and monsters. As employees of \"The Company\", players must sell enough scrap to meet a series of increasing profit quotas until they inevitably fail and the game starts over.",
+      [categories[2], categories[3]],
+      267,
+      9.99,
+      0
+    ),
+  ]);
+}
+
