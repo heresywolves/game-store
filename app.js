30d3fe9c47fbdc6af2b281c2e5ab23f31be73d6d
Howl Kimura <howlkimuradev@gmail.com>
2024-04-25 10:45:21 +0300

    Add logos and favicon middleware

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var favicon = require('serve-favicon');
+
 
 // Connect mongodb
 const mongoose = require("mongoose");
@@ -27,6 +29,9 @@ var app = express();
 
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Favicon
+app.use(favicon(path.join(__dirname, 'public', 'images', 'game-nexus-favicon-black.ico')))
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
